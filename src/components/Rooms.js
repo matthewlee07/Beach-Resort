f4002803a8d1b03c0300a1eef55a4756c688c357
Matt Lee <matthewlee07@gmail.com>
2019-09-19 18:08:44 -0400

    rooms lightbox

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import defaultImg from "../images/room-1.jpeg";
+import PropTypes from "prop-types";
 
-function Room() {
-  return <h5>hello from room</h5>;
+function Room({ room }) {
+  const { name, slug, images, price } = room;
+  return (
+    <article className="room">
+      <div className="img-container">
+        <img src={images[0] || defaultImg} alt="single room" />
+
+        <div className="price-top">
+          <h6>${price}</h6>
+          <p>per night</p>
+        </div>
+        <Link to={`/rooms/${slug}`} className="btn-primary room-link">
+          Features
+        </Link>
+      </div>
+      <p className="room-info room=link">{name}</p>
+    </article>
+  );
 }
+
+Room.protoTypes = {
+  room: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    slug: PropTypes.string.isRequired,
+    images: PropTypes.arrayOf(PropTypes.string).isRequired,
+    price: PropTypes.number.isRequired
+  })
+};
 
 export default Room;
