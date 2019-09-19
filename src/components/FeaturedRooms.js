781403efa4c2dd36de8dd3820d10d046523c565b
Matt Lee <matthewlee07@gmail.com>
2019-09-19 16:49:41 -0400

    adding loading & room => featured components, mounting data

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 import { RoomContext } from "../context";
+import Loading from "./Loading";
+import Room from "../components/Rooms";
+import Title from "./Title";
 
 class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
-    const { name, greeting } = this.context;
+    let { loading, featuredRooms: rooms } = this.context;
+    rooms = rooms.map(room => {
+      return <Room key={room.id} room={room} />;
+    });
 
     return (
-      <div>
-        {greeting}, {name}; from featured rooms
-      </div>
+      <section className="featured-rooms">
+        <Title title="featured rooms" />
+        <div className="featured-rooms-center">
+          {loading ? <Loading /> : rooms}
+        </div>
+      </section>
     );
   }
 }
---
+++ src/components/Rooms.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Room() {
+  return <h5>hello from room</h5>;
+}
+
+export default Room;
