ec22389641707d4c20f501b68132993b2d19c902
Matt Lee <matthewlee07@gmail.com>
2019-09-20 08:57:53 -0400

    filter types of rooms

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -1,7 +1,56 @@
 import React from "react";
+import { useContext } from "react";
+import { RoomContext } from "../context";
+import Title from "../components/Title";
 
-function RoomsFilter() {
-  return <div>Hello From rooms RoomsFilter</div>;
+const getUnique = (items, value) => {
+  return [...new Set(items.map(item => item[value]))];
+};
+
+function RoomsFilter({ rooms }) {
+  const context = useContext(RoomContext);
+  const {
+    handleChange,
+    type,
+    capacity,
+    price,
+    minPrice,
+    maxPrice,
+    minSize,
+    maxSize,
+    breakfast,
+    pets
+  } = context;
+
+  let types = getUnique(rooms, "type");
+  types = ["all", ...types];
+  types = types.map((item, index) => {
+    return (
+      <option value={item} key={index}>
+        {item}
+      </option>
+    );
+  });
+
+  return (
+    <section className="filter-container">
+      <Title title="search rooms" />
+      <form action="" className="filter-form">
+        <div className="form-group">
+          <label htmlFor="type">room type</label>
+          <select
+            name="type"
+            id="type"
+            value={type}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {types}
+          </select>
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default RoomsFilter;
